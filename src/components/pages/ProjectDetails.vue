90d736c583b4ef91ae5fa88d03245256bc3e1ecb
Alex <vanilaninja13@mail.ru>
2024-03-04 23:37:07 +0300

    create store vuex

--- src/components/pages/ProjectDetails.vue
+++ src/components/pages/ProjectDetails.vue
@@ -6,11 +6,9 @@
 </div>
 <div class="project__details-body center">
 <div class="project__details-body-info">
-    <h2 class="project__details-body-title">Minimal Look Bedrooms</h2>
+    <h2 class="project__details-body-title">{{ projectDetails.title }}</h2>
     <div class="project__details-body-subtitle">
-        <p class="project__details-body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquamsem vitae turpis dignissim maximus. Aliquam sollicitudin tellumassa, vbel maximus purus posuere in. Dojrices gravida dignissim. Praesent at nibh in mi fringilla mattis. Phasellus ut dolor odio. Aenean in the ipsum vel lectus bibendum commodo. 
-    </p>
-    <p class="project__details-body-text">In nec sem suscipit, convallis leo vitae, lacinia nibh. Cras amet tellus lectus. Vivamus ipsum nunc, mattis quis nibh id, pellentesque arcu. Donec a pellentesque Cras erat enim, gravida non ante vitae,elequis convallis elit, in viverra felis. Donec ultrices tellus vitae iaculisvd porta. Proin tincidunt ligula id purus porttitor.</p>
+        <p class="project__details-body-text">{{ projectDetails.description }}</p>
     </div>
 </div>
 <div class="project__details-body-swiper">
@@ -36,6 +34,7 @@
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 import Swiper from '@/components/Swiper.vue';
+import { mapGetters } from 'vuex'
 
     export default {
 
@@ -44,7 +43,9 @@ import Swiper from '@/components/Swiper.vue';
         Footer,
         Swiper,
     },
-        
+    computed: {
+    ...mapGetters(['projectDetails']),
+    },
     }
 </script>
 
